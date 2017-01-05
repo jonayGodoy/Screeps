33bfce47e8dc76dbb842d8a1d778e2d31166afb3
jonayGodoy <jonaydam2@gmail.com>
2017-01-05 22:25:56 +0100

    i play with constant

--- src/Constants.js
+++ src/Constants.js
@@ -1,25 +1,6 @@
 
 var constant = class Constants {
-     private  _HARVESTER = "harvester";
-     private  _UPGRADER = "upgrader";
-     private   _BUILDER = "builder";
 
-     constructor() {
-         this._HARVESTER = _HARVESTER;
-         this._UPGRADER = _UPGRADER;
-         this._BUILDER = _BUILDER;
-     }
-
-     public  HARVESTER() {
-         return _HARVESTER;
-     }
-
-     public get UPGRADER() {
-         return _UPGRADER;
-     }
-     public get BUILDER() {
-         return _BUILDER;
-     }
 
 };
 
