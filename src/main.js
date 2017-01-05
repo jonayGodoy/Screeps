cfd759fa8989895dc93093dc1277bbd8dae082d6
jonayGodoy <jonaydam2@gmail.com>
2017-01-05 21:18:13 +0100

    refactor main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
 var roleManager = require('RoleManager');
-
-
 
 module.exports.loop = function () {
 
@@ -29,19 +24,4 @@ module.exports.loop = function () {
 
     }
 
-
-/*
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
-    */
 }
