3410b216650d36c7b74f1ece7a02219d829ca2ff
jonayGodoy <jonaydam2@gmail.com>
2017-01-05 20:32:03 +0100

    test managerCreeps 3

--- src/RoleManager.js
+++ src/RoleManager.js
@@ -28,7 +28,7 @@ var roleManager = {
             ["builder",roleBuilder]
         ];
 
-
+        console.log( roleList[creep.memory.role]);
         roleList[creep.memory.role].run(creep);
     }
 
