a06085eb630b71e399480c7f4dc9cded077ccebf
jonayGodoy <jonaydam2@gmail.com>
2017-01-06 00:43:07 +0100

    recreate Ia

--- src/RuleHarverster.js
+++ src/RuleHarverster.js
@@ -3,11 +3,12 @@
 var ruleHarverster = class RuleHarverster{
 
     execute(){
-        console.log("hasta aqui");
         let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == constants.HARVESTER());
+        console.log("vamos "+harvesters);
         let done = false;
         if(harvesters.length < 2) {
             var info = Game.spawns["Spawn1"].createCreep([WORK,CARRY,MOVE], undefined, {role: constants.HARVESTER()});
+            console.log("info "+info);
             done = false;
             return done;
         }else{
