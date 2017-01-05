d8fdcb7a45a804820bd2c0fd1a2aa794c1e35939
jonayGodoy <jonaydam2@gmail.com>
2017-01-06 00:22:14 +0100

    test ia

--- src/rules/RuleHarverster.js
+++ src/rules/RuleHarverster.js
@@ -3,6 +3,7 @@
 var ruleHarverster = class RuleHarverster{
 
     execute(){
+        let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == constants.HARVESTER());
         let done = false;
         if(harvesters.length < 2) {
             var info = Game.spawns["Spawn1"].createCreep([WORK,CARRY,MOVE], undefined, {role: constants.HARVESTER()});
--- src/rules/RuleUpgrader.js
+++ src/rules/RuleUpgrader.js
@@ -2,6 +2,7 @@
 var ruleUpgrader = class RuleUpgrader{
 
     execute(){
+        let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == constants.UPGRADER());
         let done = false;
         if(upgraders.length < 5) {
             var info = Game.spawns["Spawn1"].createCreep([WORK,CARRY,MOVE], undefined, {role: constants.UPGRADER()});
