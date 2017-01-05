85c7b5da612171e4fdbe78f93ca400624a92a6f5
jonayGodoy <jonaydam2@gmail.com>
2017-01-06 00:55:10 +0100

    recreate Ia

--- src/EngineRules.js
+++ src/EngineRules.js
@@ -15,16 +15,13 @@ var engineRules = class EngineRules{
         let done = false;
         for(var number in  this.rulesList){
             let rule = this.rulesList[number];
-            this.executeForPriority(done, rule);
+            if (done) {
+                done = rule.execute();
+            }
         }
     }
 
-    executeForPriority(done, rule) {
-        if (done) {
-            done = rule.execute();
-        }
-        return done;
-    }
+
 }
 
 module.exports = engineRules;
