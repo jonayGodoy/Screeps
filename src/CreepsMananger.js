a83c977a3b79ee14880a2c319343773da7127f65
jonayGodoy <jonaydam2@gmail.com>
2017-01-05 20:21:20 +0100

    test managerCreeps 3

--- src/CreepsMananger.js
+++ src/CreepsMananger.js
@@ -1,23 +1,26 @@
-var roleHarvester = require('role.harvester');
+var creepsManager = {
+
+    var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 
-class CreepsManager{
-    constructor(){
-        this.roleList = [
-            ["harvester", roleHarvester],
-            ["upgrader",roleUpgrader],
-            ["builder",roleBuilder]
-        ];
+class CreepsManager {
+        constructor() {
+            this.roleList = [
+                ["harvester", roleHarvester],
+                ["upgrader", roleUpgrader],
+                ["builder", roleBuilder]
+            ];
+
+        }
+
+        runCreeper(creep) {
+            this.roleList[creep.memory.role].run(creep);
+        }
 
     }
 
-    runCreeper(creep){
-        this.roleList[creep.memory.role].run(creep);
-    }
 
-}
-var creepsManager = new CreepsManager();
-
+};
 module.exports = creepsManager;
