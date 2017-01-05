5d497386a9847c144fd8f9e45a4398afbb825cf6
jonayGodoy <jonaydam2@gmail.com>
2017-01-06 00:30:02 +0100

    recreate Ia

--- src/EngineRules.js
+++ src/EngineRules.js
@@ -1,9 +1,30 @@
-//const constants = require('Constants');
-//var RuleHarverster = require('./rules/RuleHarverster');
-//var RuleUpgrader = require('./rules/RuleUpgrader');
+const constants = require('Constants');
+var RuleHarverster = require('./rules/RuleHarverster');
+var RuleUpgrader = require('./rules/RuleUpgrader');
 
 var engineRules = class EngineRules{
+    constructor() {
+        //incidencia ordenadas por priorida
+        this.rulesList = [
+            new RuleHarverster(),
+            new RuleUpgrader
+        ];
+    }
 
+    update(){
+        let stop = false;
+        for(var number in  this.rulesList){
+            stop = this.executeForPriority(stop, number);
+        }
+    }
+
+    executeForPriority(stop, number) {
+        if (!stop) {
+            rule = rulesList[number];
+            stop = rule.execute();
+        }
+        return stop;
+    }
 }
 
 module.exports = engineRules;
