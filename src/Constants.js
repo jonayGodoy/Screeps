e1a59cde3b60f4d86ba6c7ca4d70989173747b11
jonayGodoy <jonaydam2@gmail.com>
2017-01-05 22:03:28 +0100

    i play with constant

--- src/Constants.js
+++ src/Constants.js
@@ -2,6 +2,6 @@ const HARVESTER = "harvester";
 const UPGRADER = "upgrader";
 const BUILDER = "builder";
 
-module.exports.HARVESTER  = global.HARVESTER;
-module.exports.UPGRADER = global.UPGRADER;
-module.exports.BUILDER = global.BUILDER;
+module.exports  = HARVESTER;
+module.exports = UPGRADER;
+module.exports = BUILDER;
