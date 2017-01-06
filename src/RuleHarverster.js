b7f1174d5a5fa654b239e983d08139ae0cead082
jonayGodoy <jonaydam2@gmail.com>
2017-01-06 17:18:13 +0100

    Refactoring RuleHarvester

--- src/RuleHarverster.js
+++ src/RuleHarverster.js
@@ -12,32 +12,35 @@ var ruleHarverster = class RuleHarverster{
     }
 
 
-
     setDone(done){
         this.done = done;
     }
 
     execute(){
         if(!this.done){
-
-            let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == constants.HARVESTER());
-
-            var firstSpawn;
-            for(var name in Game.spawns){
-                if(firstSpawn == null)
-                    firstSpawn = Game.spawns[name];
-            }
-
-            if(harvesters.length < 2) {
-                var info = firstSpawn.createCreep([WORK, CARRY, MOVE], undefined, {role: constants.HARVESTER()});
-                this.done = false;
-            }else{
-                this.done = true;
-            }
-
-
+            this.done = this.behaviorRule();
         }
         return this.done;
+    }
+
+    behaviorRule() {
+        let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == constants.HARVESTER());
+
+        let conditionDone = (harvesters.length >= 2);
+
+
+        var firstSpawn;
+        for (var name in Game.spawns) {
+            if (firstSpawn == null)
+                firstSpawn = Game.spawns[name];
+        }
+
+
+        if (!conditionDone) {
+            var info = firstSpawn.createCreep([WORK, CARRY, MOVE], undefined, {role: constants.HARVESTER()});
+        } else {
+            this.done = conditionDone;
+        }
     }
 }
 module.exports = ruleHarverster;
