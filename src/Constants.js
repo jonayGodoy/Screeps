02ef23e56192833c0a6f62b6ff0ef6de96e58bc8
jonayGodoy <jonaydam2@gmail.com>
2017-01-08 00:19:26 +0100

    remove some warning

--- src/Constants.js
+++ src/Constants.js
@@ -6,7 +6,7 @@
          this.harvester = "harvester";
          this.upgrader = "upgrader";
          this.builder = "builder";
-         this.monitor = "";
+         this.monitor = "monitor";
      }
 
      HARVESTER() {
