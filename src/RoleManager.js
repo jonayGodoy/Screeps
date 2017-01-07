c45a27a6b88807c17947babeed7a9080b78bb061
jonayGodoy <jonaydam2@gmail.com>
2017-01-08 00:33:17 +0100

    add RoleMonitor

--- src/RoleManager.js
+++ src/RoleManager.js
@@ -16,7 +16,7 @@ module.exports = class RoleManager{
         console.log(roleMonitor.run);
         console.log(roleUpgrader.run);
         this.roleList[constants.MONITOR()] = roleMonitor;
-        console.log("message "+this.roleList[constants.MONITOR().run(undefined)]);
+        console.log("message "+this.roleList[constants.MONITOR()].run(undefined));
 
 
     }
