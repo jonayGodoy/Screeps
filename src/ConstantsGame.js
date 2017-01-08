f4bd444fcfb721fa378f7d5770fe500b8f4104fd
jonayGodoy <jonaydam2@gmail.com>
2017-01-08 14:23:15 +0100

    add calculateParts

--- src/ConstantsGame.js
+++ src/ConstantsGame.js
@@ -2,13 +2,14 @@ class ConstantsGame {
 
     constructor() {
         this.listPartsCreep = [];
-        this.listPartsCreep[MOVE] = [50, "Decreases fatigue by 2 points per tick."];
+        this.listPartsCreep[MOVE] = [50, "Decreases fatigue by 2 points per tick and affect velocity"];
         this.listPartsCreep[CARRY] = [50, "Decreases fatigue by 2 points per tick."];
         this.listPartsCreep[WORK] = [100, "it can use the methods harvest (), build (), repair, dismantle () and upgradeController ()" +
         "and improvement methods "];
     }
 
     getCostPartCreep(part){
+        console.log(this.listPartsCreep[part]);
        return (this.listPartsCreep[part])[0];
     }
 
