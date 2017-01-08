1c1c35581f505838482921c13d1e247f4df26ec2
jonayGodoy <jonaydam2@gmail.com>
2017-01-08 02:44:01 +0100

    add RoleMonitor

--- src/RoleMonitor.js
+++ src/RoleMonitor.js
@@ -6,13 +6,18 @@ module.exports = class RoleMonitor{
 
     run(creep){
         for(var number in this.message){
+            this.sleep(500);
             creep.say(this.message[number]);
         }
     }
 
+    sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
+    }
+
     creepDelaySay(creep,number){
         console.log("funciona");
-        creep.say(this.message[number]);
     }
 
     creepMonitorPrint(message){
