99f3a63e3f697dd7267d049bd9759a94daea69cf
jonayGodoy <jonaydam2@gmail.com>
2017-01-08 19:08:25 +0100

    Rule renovate  error Could not find an object with ID

--- src/CallGame.js
+++ src/CallGame.js
@@ -58,8 +58,9 @@ class CallGame{
     cleanMemoryCreep() {
         console.log("memoria creeps "+Memory.creeps);
         for (var name in Memory.creeps) {
+            console.log("creeps  en la memoria "+name);
             if (!Game.creeps[name]) {
-                console.log("creeps  en la memoria "+name);
+                console.log("creeps Muertos en la memoria "+name);
                 delete Memory.creeps[name];
             }
         }
