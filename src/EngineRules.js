17b4c24bc9b07fc5c13d631822382b80218d7357
jonay <casa@gmail.com>
2017-01-09 11:52:42 +0000

    add Dao

--- src/EngineRules.js
+++ src/EngineRules.js
@@ -11,14 +11,15 @@ module.exports = class EngineRules{
         this.rulesListPasivesSortedByPriority = rulesListPasivesSortedByPriority;
 
         this.dao = new Dao();
-        this.dao.loadRuleListActives(rulesListActivesSortedByPriority);
-
+      //  this.dao.loadRuleListActives(rulesListActivesSortedByPriority);
+        this.loadRuleListActives(rulesListActivesSortedByPriority);
     }
 
     updateRules(){
         this.updateRuleActiveListForPriority();
         this.updateRulePasiveListForPriority();
-        this.dao.saveRuleListActive();
+     //  this.dao.saveRuleListActive();
+        this.saveRuleListActive();
 
     }
 
