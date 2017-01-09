781a15e1f7809dd46789ac220cb5918844d846ce
jonay <casa@gmail.com>
2017-01-09 20:07:06 +0000

    add  call iaMain to callGame

--- src/CallGame.js
+++ src/CallGame.js
@@ -79,8 +79,8 @@ class CallGame{
         for (var name in Memory.creeps) {
          //   console.log("creeps  en la memoria "+name);
         //    console.log("creeps Muertos en la memoria "+name+ "  ticks" +Game.creeps[name].ticksToLive);
-            if (Game.creeps[name] != undefined) {
-                console.log("creeps Muertos en la memoria "+name+ "  ticks" +Game.creeps[name].ticksToLive);
+            if (Game.creeps[name] != undefined && Game.creeps[name].ticksToLive === undefined) {
+                console.log("creeps Muertos en la memoria "+name+ "  ticks " +Game.creeps[name].ticksToLive);
                 delete Memory.creeps[name];
             }
         }
