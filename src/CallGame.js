f599acde2a68e4c86c8b4cc44bad2c4c1c7f7aa5
jonay <casa@gmail.com>
2017-01-09 18:49:48 +0000

    add  call iaMain to callGame

--- src/CallGame.js
+++ src/CallGame.js
@@ -105,7 +105,7 @@ class CallGame{
 
 
     ticksWithDelayPARCHE(name){
-        let delayTicksCreeps = 10;
+        let delayTicksCreeps = 40;
         if(Game.time % delayTicksCreeps == 0) {
             return Game.creeps[name].ticksToLive;
         }
