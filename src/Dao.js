4f935ed5619c15e6047bd1a31c653dd782decea1
jonayGodoy <jonaydam2@gmail.com>
2017-01-10 01:12:52 +0100

    reparing dao

--- src/Dao.js
+++ src/Dao.js
@@ -1,24 +1,9 @@
 var callGame = require('CallGame');
 module.exports = class Dao{
 
-    saveListCreepData(rulesListCreepData){
-        callGame.getFirstSpawn().room.memory.stateIAListCreepData = rulesListCreepData;
-    }
-
-
-    loadListCreepData(listCreepData){
-        let firstSpawn = callGame.getFirstSpawn();
-     //   console.log("llamada load "+firstSpawn.room.memory.stateIAListCreepData);
-
-        if (firstSpawn.room.memory.stateIAListCreepData != undefined ) {
-             listCreepData = firstSpawn.room.memory.stateIAListCreepData;
-        }
-        return listCreepData;
-    }
 
 
     saveRuleListActive(rulesListActivesSortedByPriority) {
-
         callGame.getFirstSpawn().room.memory.stateIARuleActive = rulesListActivesSortedByPriority;
     }
 
