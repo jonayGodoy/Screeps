75fdc8d4331c971188a2e92625d907077ec85cd2
jonayGodoy <jonaydam2@gmail.com>
2017-01-11 06:49:07 +0100

    i start refactor RuleBuildExtension

--- src/Dao.js
+++ src/Dao.js
@@ -17,7 +17,7 @@ module.exports = class Dao{
     loadRenovateCreep(){
         let firstSpawn = callGame.getFirstSpawn();
 
-        if (firstSpawn.room.memory.RenovatingCreep["name"] != undefined &&  firstSpawn.room.memory.RenovatingCreep["role"]  != undefined) {
+        if ((firstSpawn.room.memory.RenovatingCreep  != undefined) || (firstSpawn.room.memory.RenovatingCreep["name"] != undefined &&  firstSpawn.room.memory.RenovatingCreep["role"]  != undefined)) {
                 let creepOldMemory = firstSpawn.room.memory.RenovatingCreep;
             console.log("name----------------"+creepOldMemory.name+"---------------------");
             console.log("role----------------"+creepOldMemory.role+"---------------------");
