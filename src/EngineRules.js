ac64fc59cea84a0fdfa1f1c2da24da770fa792d4
jonayGodoy <jonaydam2@gmail.com>
2017-01-13 01:46:59 +0100

    refactor EngineRules

--- src/EngineRules.js
+++ src/EngineRules.js
@@ -32,7 +32,7 @@ module.exports = class EngineRules{
             let ruleAddState = this.rulesListActivesSortedByPriority[index];
             ruleAddState[constants.RULE_ACTIVE_STATE()] = this.executeRuleOnce(ruleAddState);
             stopList = !ruleAddState[constants.RULE_ACTIVE_STATE()];
-            console.log(" estado "+stopList);
+         //   console.log(" estado "+stopList);
             if(stopList){
                 this.printState(ruleAddState[constants.RULE_ACTIVE()]);
             }
