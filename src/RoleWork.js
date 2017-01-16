90e9e47f15ddeaab32f7b0aed9ca16691513aa67
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 11:28:26 +0100

    refactor roles RoleWork

--- src/RoleWork.js
+++ src/RoleWork.js
@@ -6,26 +6,33 @@ module.exports = class RoleWork{
 
     run(creep){
         if(creep.carry.energy < creep.carryCapacity) {
-            this.recharge = true;
+            this.changeRecharge(true,creep)
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
-            this.recharge = false;
+            this.changeRecharge(false,creep)
             this.roleObject.run(creep);
 
         }
 
-        if(this.recharge){
-            creep.say("Recharge");
-        }else{
-            creep.say("Work");
-        }
+
     }
 
+    changeRecharge(bool,creep){
 
+        if(this.recharge != bool){
+            if(this.recharge){
+                creep.say("Recharge");
+            }else{
+                creep.say("Work");
+            }
+            this.recharge = bool;
+        }
+
+    }
 
 
 };
