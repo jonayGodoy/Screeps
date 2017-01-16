a9d5cbecfd2a191cf970abb9a5e43331f84f7440
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 12:23:38 +0100

    refactor roles RoleWork

--- src/RoleHarvester.js
+++ src/RoleHarvester.js
@@ -1,6 +1,18 @@
 var RoleWork_Abstract = require('RoleWork_Abstract');
 module.exports = class RoleHarvester extends RoleWork_Abstract{
 
+
+    changeWork(creep){
+        if(creep.memory.working && creep.carry.energy == 0) {
+            creep.memory.working = false;
+            creep.say("recharge");
+        }
+        if(!creep.memory.working && creep.carry.energy < creep.carryCapacity) {
+            creep.memory.working = true;
+            creep.say("working");
+        }
+    }
+
     work(creep){
         var targets = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
