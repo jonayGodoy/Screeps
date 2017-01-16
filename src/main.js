1c1f92c07b6800d93175057812b70f3a42d3df5f
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 10:57:39 +0100

    refactor EngineRules y condition rules to true

--- src/main.js
+++ src/main.js
@@ -2,19 +2,12 @@ var ia = require('IAMain');
 var roleManager = require('RoleManager');
 var callGame = require('CallGame');
 //se resetea por el serve
-var RoleMonitor = require('RoleMonitor');
+
 
 module.exports.loop = function () {
 
     ia.updateIA();
     creepsUpdate();
-
-    let serialize = JSON.stringify(new RoleMonitor());
-    let obj = JSON.parse(serialize);
-    console.log(obj.run);
-
-
-
 
 };
 
