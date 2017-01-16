fdd26cb8a03e10a78207804de65f4d43cb83d900
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 11:51:00 +0100

    refactor roles RoleWork

--- src/RoleWork.js
+++ src/RoleWork.js
@@ -1,34 +1,33 @@
 module.exports = class RoleWork{
     constructor(roleObject) {
         this.roleObject = roleObject;
-        this.isRecharge = false;
     }
 
     run(creep){
-        if(!this.isRecharge && creep.carry.energy == 0) {
-            this.changeRecharge(true,creep);
-            var sources = creep.room.find(FIND_SOURCES);
+        this.changeWork(creep);
+
+        if(creep.memory.working) {
+            this.roleObject.run(creep);
+        }
+        else {
+            let sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
-        else {
-            if(creep.carry.energy == creep.carryCapacity && this.isRecharge) {
-                this.changeRecharge(false, creep);
-                this.roleObject.run(creep);
-            }
-        }
+
     }
 
-    changeRecharge(bool,creep){
-        if(this.isRecharge != bool){
-            this.isRecharge = bool;
-            if(this.isRecharge){
-                creep.say("Recharge");
-            }else{
-                creep.say("Work "+creep.name);
-            }
+    changeWork(creep){
+        if(creep.memory.working && creep.carry.energy == 0) {
+            creep.memory.working = false;
+            creep.say('harvesting');
         }
+        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+            creep.say('building');
+        }
+
 
     }
 
