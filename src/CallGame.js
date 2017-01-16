a4225dc289e3a1e4c5037460894aa306874b4c09
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 07:31:21 +0100

    refactor EngineRules y condition rules to true

--- src/CallGame.js
+++ src/CallGame.js
@@ -56,8 +56,7 @@ class CallGame{
 
 
     findCreepersForRole(role){
-
-        return _.filter(Game.creeps, (creep) => creep.memory.role == role) && _.filter(Game.creeps, (creep) => creep.ticksToLive > 0) && _.filter(Game.creeps, (creep) => creep.ticksToLive < 1500);
+        return _.filter(Game.creeps, ((creep) => creep.memory.role == role && creep.ticksToLive < 1500));
     }
 
 
