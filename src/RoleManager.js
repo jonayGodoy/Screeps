69a1d78206a0eea1df0badef73d1344c7a9125d9
jonayGodoy <jonaydam2@gmail.com>
2017-01-16 11:33:43 +0100

    refactor roles RoleWork

--- src/RoleManager.js
+++ src/RoleManager.js
@@ -11,7 +11,7 @@ class RoleManager{
         this.roleList = [];
         this.roleList[constants.HARVESTER()] = new RoleWork(new RoleHarvester());
         this.roleList[constants.UPGRADER()] = new RoleWork(new RoleUpgrader());
-        this.roleList[constants.BUILDER()] = new RoleBuilder();
+        this.roleList[constants.BUILDER()] = new RoleWork(new RoleBuilder());
         this.roleList[constants.MONITOR()] =  new RoleMonitor();
     }
 
