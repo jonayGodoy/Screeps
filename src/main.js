03843ba848ffccb0c087f6d8c81aa52cda0fd0d6
jonay <casa@gmail.com>
2017-02-15 16:10:45 +0000

    i start to refactor code for test

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
-var ia = require('IAMain');
-var roleManager = require('RoleManager');
-var callGame = require('CallGame');
 //se resetea por el serve
 var main = require('MainClass');
 
 module.exports.loop = function () {
     main.mainExecute();
 };
-
-
-
-    /*
-function creepsUpdate() {
-    for (var name in callGame.getCreeps()) {
-        var creep = callGame.getCreeps()[name];
-        roleManager.runCreeps(creep);
-    }
-}
- */
